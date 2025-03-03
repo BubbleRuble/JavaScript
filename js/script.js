7ec86305bd4ce5c9552d48d03cfadfa23339ac96
Andriy <jeki4an22@gmail.com>
2025-03-03 20:26:22 +0200

    js begin

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const a = 'Зміна *a* в script.js'
+
+console.log(a);
+
+const b = 'Зміна *b* в script.js'
+
+console.log(b);
+
+const c = 'Зміна *c* в script.js'
+
+console.log(c);
+
+const isOpen = false;
+
+const isClosed = true;
+
+
+let age = 18;
+
+console.log('Age', age);
+
+const type = typeof isOpen;
+
+console.log(type);
+
+const abc = typeof isClosed;
+
+console.log(abc);
+
+alert()
+
+
+
