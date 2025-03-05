d63295d9e4b26786368732974108e8d7361304c0
Andriy <jeki4an22@gmail.com>
2025-03-05 18:39:31 +0200

    JavaScript continue

--- js/numbers.js
+++ js/numbers.js
@@ -1,22 +1,43 @@
-let elementWidth = '50px';
-const result = Number.parseInt(elementWidth);
-console.log(result);
-// elementWidth = ;
-// console.log('elementWidth: ');
+// let elementWidth = '50px';
+// elementWidth = Number.parseInt(elementWidth);
+// console.log('elementWidth:', elementWidth);
 
-let elementHeight = '341.22';
-// elementHeight = ;
-// console.log('elementHeight:')
+// let elementHeight = '341.22px';
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log('elementHeight:', elementHeight);
 
-const salary = 1200.12423;
+// let salary = 1200.12345;
+// salary = Number(salary.toFixed(2));
 
-let quantity = '30';
-let value = 'Цей рядок неможливо привести до числа';
+// console.log(salary);
+// console.log(Number(salary.toFixed(2)));
+
+// let quantity = '30';
+// let value = 'Цей рядок неможливо привести до числа';
+
+// console.log(Number(quantity));
+
+// console.log(Math.E)
 
 
-const base = 2;
-const power = 5;
+// const base = 2;
+// const power = 5;
 
 
-const app = '50';
-console.log(app);
+
+// console.log(Math.pow(base, power));
+// console.log(power ** base);
+// console.log(Math.sqrt(49))
+// console.log(Math.pow(5,0.5));
+
+let base = prompt('Введіть число');
+base = Number(base);
+console.log(base);
+
+let power = prompt('Введіть ступінь');
+power = Number(power);
+console.log(power);
+
+console.log(Math.pow(base,power));
+
+
