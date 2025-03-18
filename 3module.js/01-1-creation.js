e2e7e3a91e358c151368ca2f86b140707366a2b8
Andriy <jeki4an22@gmail.com>
2025-03-18 14:06:17 +0200

    begin 3 module

---
+++ 3module.js/01-1-creation.js
@@ -0,0 +1,57 @@
+const playlist = {
+  name: 'my super playlist',
+  rating: 5,
+  tracks: ['Bejelewed', 'Blank Space', 'TGIF', 'Fortnite balls'],
+  trackCount: 4,
+};
+
+playlist.qwe = 5;
+
+
+// console.log(playlist.name);
+// console.log(playlist.tracks);
+
+// const propertyName = 'tracks';
+
+// console.log(playlist.propertyName);
+// console.log(playlist[propertyName]);
+
+// const username = 'Mango';
+// const email = 'mango@mail.com';
+// const password = 'Ann1f1gm4';
+
+// const signupData = {
+//   username, //===  username: username, 
+//   email,
+//   password,
+// };
+
+// console.log(signupData);
+
+// const inputName = 'color';
+// const inputValue = 'tomato';
+
+// const colorPickerData = {
+//   [inputName]:inputValue,
+// };
+
+// console.log(colorPickerData);
+
+
+// const a = { x:1, y:2};
+// const b = a;
+
+// console.log(b === a);
+
+// a.hello = 100;
+// a.hello = 300;
+
+// console.log(a);
+// console.log(b);
+
+
+
+const a = [1, 2, 3];
+
+a.push(4);
+console.log(a);
